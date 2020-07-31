7edac8e3d697fecc8ccd0708840f5c12aeedb53d
Josh Raiborde <joshsrai@yahoo.com>
2020-07-31 14:57:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,6 @@ require "pry"
 def reverse_each_word(string)
 string = string.split
 reverse = string.map {|word| word.reverse}
-binding.pry
-reversed = []
-string.each {|i| reversed.push(string.pop)}
-return reversed.join(" ")
+return reverse.join(" ")
 end
 reverse_each_word("Hello, and how are you doing?")
