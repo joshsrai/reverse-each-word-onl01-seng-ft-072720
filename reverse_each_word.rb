da61fcbd5daeec6d43cd46c9a64257f0117c9d77
Josh Raiborde <joshsrai@yahoo.com>
2020-07-31 14:54:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 require "pry"
 def reverse_each_word(string)
-string = string.split(" ")
+string = string.split
+reverse = string.map {|word| word.reverse}
 binding.pry
 reversed = []
 string.each {|i| reversed.push(string.pop)}
