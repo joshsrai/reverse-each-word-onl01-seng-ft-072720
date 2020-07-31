aaf8629ff325fb5f86fcc7662a0c5c48d856d01f
Josh Raiborde <joshsrai@yahoo.com>
2020-07-31 14:56:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,6 +5,6 @@ reverse = string.map {|word| word.reverse}
 binding.pry
 reversed = []
 string.each {|i| reversed.push(string.pop)}
-return reversed.join("")
+return reversed.join(" ")
 end
 reverse_each_word("Hello, and how are you doing?")
