b4fb74e52111ae6d36aa6b544652acb9578fa1ab
Josh Raiborde <joshsrai@yahoo.com>
2020-07-31 14:58:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 require "pry"
 def reverse_each_word(string)
 string = string.split
-reverse = string.map {|word| word.reverse}
+reverse = string.collect {|word| word.reverse}
 return reverse.join(" ")
 end
 reverse_each_word("Hello, and how are you doing?")
