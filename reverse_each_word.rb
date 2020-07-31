a4e360169df804951a6f3c21a1bc275c25d6f403
Josh Raiborde <joshsrai@yahoo.com>
2020-07-31 14:46:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,7 @@
+require "pry"
 def reverse_each_word(string)
-string = string.split("")
+string = string.split(" ")]
+bindng.pry
 reversed = []
 string.each {|i| reversed.push(string.pop)}
 return reversed.join("")
